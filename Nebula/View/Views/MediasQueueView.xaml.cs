7d8766e7f106c57b64a54eb91d94d0d0b2486bd3
Unreal <dodo-pl-08@hotmail.fr>
2021-06-02 21:22:43 +0200

    MediasQueueView.xaml

---
+++ Nebula/View/Views/MediasQueueView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Nebula.View.Views
+{
+    public partial class MediasQueueView : UserControl
+    {
+        public MediasQueueView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Nebula/ViewModel/MediasQueueViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LiteMVVM;
+using Nebula.Media;
+using Nebula.Model;
+
+namespace Nebula.ViewModel
+{
+    public class MediasQueueViewModel : BaseViewModel
+    {
+        public MediasQueueViewModel()
+        {
+            NebulaClient.MediaPlayer.MediaChanged += (_, _) => { OnPropertiesChanged(nameof(MediaTitle), nameof(MediaAuthor)); };
+        }
+
+        public IMediaInfo CurrentMedia => NebulaClient.MediaPlayer.CurrentMedia;
+
+        public string MediaTitle  => CurrentMedia?.Title ?? "Unknown";
+        public string MediaAuthor => CurrentMedia?.Author ?? "Unknown";
+    }
+}
